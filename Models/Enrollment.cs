9fea43f961a0ae1c181ee99b2c3611c9bfebee48
SrishtiSk <unofficial.srishtisk@gmail.com>
2020-12-24 00:06:26 +0530

    part 1
    create db, scafolding,

---
+++ Models/Enrollment.cs
@@ -0,0 +1,19 @@
+﻿namespace ContosoUniversity.Models
+{
+    public enum Grade { A, B, C, D, F}
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        public Grade? Grade { get; set; } /// <summary>
+                                          /// The Grade property is an enum. The ? after the Grade type 
+                                          /// declaration indicates that the Grade property is nullable.
+                                          /// A grade that's null is different from a zero grade.
+                                          /// null means a grade isn't known or hasn't been assigned yet.
+                                          /// </summary>
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
